b738e89edf5d883f945e8c2449ffcaf0a2b01d2f
aravindhan <aravindhanr552@gmail.com>
2023-01-07 22:21:25 +0530

    test case correction for attachment upload method

--- src/test/java/com/example/jobboardapp/serviceimpl/AttachementServiceImplTest.java
+++ src/test/java/com/example/jobboardapp/serviceimpl/AttachementServiceImplTest.java
@@ -1,10 +1,7 @@
 package com.example.jobboardapp.serviceimpl;
 
-import static org.junit.jupiter.api.Assertions.assertSame;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.any;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 import com.example.jobboardapp.dao.IAttachmentDao;
 import com.example.jobboardapp.dao.IJobSeekerDao;
@@ -43,54 +40,35 @@ class AttachementServiceImplTest {
      * Method under test: {@link AttachementServiceImpl#uploadResume(MultipartFile, Long)}
      */
 
-    /*
     @Test
     void testUploadResume() throws Exception {
-        Attachment attachment = new Attachment();
-        attachment.setData("AAAAAAAA".getBytes("UTF-8"));
-        attachment.setFileName("foo.txt");
-        attachment.setFileType("File Type");
-        attachment.setId("42");
-        attachment.setJobSeeker(new JobSeeker());
 
+        // Set up mock multipartFile object
+        MultipartFile mockMultipartFile = mock(MultipartFile.class);
+        when(mockMultipartFile.getOriginalFilename()).thenReturn("resume.pdf");
+        when(mockMultipartFile.getContentType()).thenReturn("application/pdf");
+        when(mockMultipartFile.getBytes()).thenReturn("content".getBytes());
+
+        // Set up expected return value
+        Attachment expectedAttachment = new Attachment("resume.pdf", "application/pdf", "content".getBytes());
         JobSeeker jobSeeker = new JobSeeker();
-        jobSeeker.setAppliedJobs(new ArrayList<>());
-        jobSeeker.setAttachment(attachment);
-        jobSeeker.setEmail("jane.doe@example.org");
-        jobSeeker.setFirstName("Jane");
-        jobSeeker.setId(123L);
-        jobSeeker.setLastName("Doe");
-        jobSeeker.setPassword("iloveyou");
-        jobSeeker.setSkill("Skill");
+        jobSeeker.setId(1L);
+        expectedAttachment.setJobSeeker(jobSeeker);
 
-        Attachment attachment1 = new Attachment();
-        attachment1.setData("AAAAAAAA".getBytes("UTF-8"));
-        attachment1.setFileName("foo.txt");
-        attachment1.setFileType("File Type");
-        attachment1.setId("42");
-        attachment1.setJobSeeker(jobSeeker);
+        // Set up mock attachmentDao to return expectedAttachment when save method is called
+        when(iAttachmentDao.save(any(Attachment.class))).thenReturn(expectedAttachment);
 
-        JobSeeker jobSeeker1 = new JobSeeker();
-        jobSeeker1.setAppliedJobs(new ArrayList<>());
-        jobSeeker1.setAttachment(attachment1);
-        jobSeeker1.setEmail("jane.doe@example.org");
-        jobSeeker1.setFirstName("Jane");
-        jobSeeker1.setId(123L);
-        jobSeeker1.setLastName("Doe");
-        jobSeeker1.setPassword("iloveyou");
-        jobSeeker1.setSkill("Skill");
+        // Set up mock jobSeekerDao to return true when existsById method is called
+        when(iJobSeekerDao.existsById(anyLong())).thenReturn(true);
+        // Set up mock attachmentDao to return null when findByJobSeekerId method is called
+        when(iAttachmentDao.findByJobSeekerId(anyLong())).thenReturn(null);
 
-        Attachment attachment2 = new Attachment();
-        attachment2.setData("AAAAAAAA".getBytes("UTF-8"));
-        attachment2.setFileName("foo.txt");
-        attachment2.setFileType("File Type");
-        attachment2.setId("42");
-        attachment2.setJobSeeker(jobSeeker1);
-        when(iAttachmentDao.save((Attachment) any())).thenReturn(attachment2);
-        assertSame(attachment2, attachementServiceImpl
-                .uploadResume(new MockMultipartFile("Name", new ByteArrayInputStream("AAAAAAAA".getBytes("UTF-8"))), 123L));
-        verify(iAttachmentDao).save((Attachment) any());
-    }*/
+        // Call uploadResume and store result
+        Attachment result = attachementServiceImpl.uploadResume(mockMultipartFile, 1L);
+
+        // Verify that result is as expected
+        assertEquals(expectedAttachment, result);
+    }
 
     /**
      * Method under test: {@link AttachementServiceImpl#getAttachment(Long)}
